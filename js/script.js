938e9de92abbfb5603950e9ba6e00a77c8d0562a
Josh <jrstaples19@gmail.com>
2022-08-17 14:24:45 -0500

    Implement operate function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,19 @@ function divide(a, b)
 {
     return a/b;
 }
+
+function operate(operator, a, b)
+{
+    switch(operator)
+    {
+        case '-': return a-b; break;
+        case '+': return a+b; break;
+        case '*': return a*b; break;
+        case '/': return a/b;
+    }
+}
 console.log(add(2,2))
 console.log(subtract(2,2))
 console.log(multiply(2,2))
 console.log(divide(2,2))
+console.log(operate('+', 2, 6));
